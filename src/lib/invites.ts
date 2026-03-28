8b22c0098fab13ce2878409bad4655a83cb7563a
Donovan Townes <donny81888@hotmail.com>
2026-03-28 09:48:02 -0500

    fix: explicitly type prisma transaction callback parameter

--- src/lib/invites.ts
+++ src/lib/invites.ts
@@ -94,7 +94,8 @@ export async function acceptInvite(
     );
   }
 
-  return prisma.$transaction(async (tx) => {
+  type PrismaTransaction = Parameters<Parameters<typeof prisma.$transaction>[0]>[0];
+  return prisma.$transaction(async (tx: PrismaTransaction) => {
     const existingMembership = await tx.membership.findUnique({
       where: {
         workspaceId_userId: {
